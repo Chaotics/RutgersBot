3e6acaf27f6bad366db059d01df2def9ec5b2b8c
Anarav <10067468+Anarav@users.noreply.github.com>
2020-01-22 23:41:32 -0500

    Reformat hours display

--- cogs/cave.py
+++ cogs/cave.py
@@ -13,28 +13,12 @@ class Cave(commands.Cog):
 
     @commands.command()
     async def Cave(self, ctx):
-        currentDT = datetime.today().strftime('%A')
-        # print(currentDT)
-        embed = discord.Embed(title="Cave Times", description="", color=COLOR_RED)
-        if currentDT == "Monday":
-            str_to_send = "Monday: 1PM - 11PM\n"
-        elif currentDT == "Tuesday":
-            str_to_send = " Tuesday: 1PM - 11PM\n"
-        elif currentDT == "Wednesday":
-            str_to_send = " Wednesday: 1PM - 11PM\n"
-        elif currentDT == "Thursday":
-            str_to_send = " Thursday: 1PM - 11PM\n"
-        elif currentDT == "Friday":
-            str_to_send = " Friday: 1PM - 6PM\n"
-        elif currentDT == "Saturday":
-            str_to_send = " Saturday: Closed\n"
-        else:
-            str_to_send = " Sunday: 3PM - 11PM\n"
-
-        # old string to send str_to_send = mon_time_str + tues_time_str + wed_time_str + thurs_time_str +
-        # fri_time_str + sat_time_str + sun_time_str
-
-        embed.add_field(name="The hours for today are:", value=str_to_send, inline=False)
+        today = datetime.today().weekday()
+        times = ["Monday: 1PM - 11PM", "Tuesday: 1PM - 11PM", "Wednesday: 1PM - 11PM", "Thursday: 1PM - 11PM",
+                 "Friday: 1PM - 6PM", "Saturday: Closed", "Sunday: 3PM - 11PM"]
+        times[today] = f"**{times[today]}**"
+        str_to_send = "\n".join(times)
+        embed = discord.Embed(title="Cave Times", description=str_to_send, color=COLOR_RED)
         await ctx.send(embed=embed)
 
 
--- cogs/dining.py
+++ cogs/dining.py
@@ -1,76 +1,134 @@
-from datetime import datetime
+from datetime import datetime, time
 
 import discord
+import pytz
 from discord.ext import commands
 
 from turn_on import COLOR_RED
 
 
 class Dining(commands.Cog):
+    eastern_timezone = pytz.timezone("US/Eastern")
+    time_format = "%I:%M %p"
+    takeout_times = {
+        "breakfast_start": time(hour=7, tzinfo=eastern_timezone),
+        "breakfast_end": time(hour=10, minute=30, tzinfo=eastern_timezone),
+        "lunch_start": time(hour=11, minute=30, tzinfo=eastern_timezone),
+        "lunch_end": time(hour=16, tzinfo=eastern_timezone),
+        "weekday_dinner_start": time(hour=17, tzinfo=eastern_timezone),
+        "weekday_dinner_end": time(hour=0, tzinfo=eastern_timezone),
+        "sunday_dinner_start": time(hour=17, minute=30, tzinfo=eastern_timezone),
+        "sunday_dinner_end": time(hour=22, tzinfo=eastern_timezone),
+        "breakfast_time": None,
+        "lunch_time": None,
+        "weekday_dinner_time": None,
+        "sunday_dinner_time": None
+    }
+    takeout_times[
+        "breakfast_time"] = f"Monday-Friday: {takeout_times['breakfast_start'].strftime(time_format)} - " \
+                            f"{takeout_times['breakfast_end'].strftime(time_format)}"
+    takeout_times[
+        "lunch_time"] = f"Monday-Friday: {takeout_times['lunch_start'].strftime(time_format)} - " \
+                        f"{takeout_times['lunch_end'].strftime(time_format)}"
+    takeout_times[
+        "weekday_dinner_time"] = f"Monday-Thursday: {takeout_times['weekday_dinner_start'].strftime(time_format)} - " \
+                                 f"{takeout_times['weekday_dinner_end'].strftime(time_format)} "
+    takeout_times[
+        "sunday_dinner_time"] = f"Sunday: {takeout_times['sunday_dinner_start'].strftime(time_format)} - " \
+                                f"{takeout_times['sunday_dinner_end'].strftime(time_format)}"
+
+    dinein_times = {
+        "weekday_breakfast_start": time(hour=7, tzinfo=eastern_timezone),
+        "weekday_breakfast_end": time(hour=11, tzinfo=eastern_timezone),
+        "weekday_lunch_start": time(hour=11, tzinfo=eastern_timezone),
+        "weekday_lunch_end": time(hour=16, tzinfo=eastern_timezone),
+        "weekday_dinner_start": time(hour=16, tzinfo=eastern_timezone),
+        "weekday_dinner_end": time(hour=21, tzinfo=eastern_timezone),
+        "weekend_brunch_start": time(hour=9, minute=30, tzinfo=eastern_timezone),
+        "weekend_brunch_end": time(hour=16, tzinfo=eastern_timezone),
+        "weekend_dinner_start": time(hour=16, tzinfo=eastern_timezone),
+        "weekend_dinner_end": time(hour=20, tzinfo=eastern_timezone),
+        "weekday_times": {
+            "weekday_breakfast_time": None,
+            "weekday_lunch_time": None,
+            "weekday_dinner_time": None
+        },
+        "weekend_times": {
+            "weekend_brunch_time": None,
+            "weekend_dinner_time": None
+        }
+    }
+    dinein_times["weekday_times"][
+        "weekday_breakfast_time"] = f"Breakfast: {dinein_times['weekday_breakfast_start'].strftime(time_format)} - " \
+                                    f"{dinein_times['weekday_breakfast_end'].strftime(time_format)}"
+    dinein_times["weekday_times"][
+        "weekday_lunch_time"] = f"Lunch: {dinein_times['weekday_lunch_start'].strftime(time_format)} - " \
+                                f"{dinein_times['weekday_lunch_end'].strftime(time_format)}"
+    dinein_times["weekday_times"][
+        "weekday_dinner_time"] = f"Dinner: {dinein_times['weekday_dinner_start'].strftime(time_format)} - " \
+                                 f"{dinein_times['weekday_dinner_end'].strftime(time_format)}"
+    dinein_times["weekend_times"][
+        "weekend_brunch_time"] = f"Brunch: {dinein_times['weekend_brunch_start'].strftime(time_format)} - " \
+                                 f"{dinein_times['weekend_brunch_end'].strftime(time_format)}"
+    dinein_times["weekend_times"][
+        "weekend_dinner_time"] = f"Dinner: {dinein_times['weekend_dinner_start'].strftime(time_format)} - " \
+                                 f"{dinein_times['weekend_dinner_end'].strftime(time_format)}"
 
     def __init__(self, client):
         self.client = client
 
     @commands.command()
     async def Takeout(self, ctx):
-        embed = discord.Embed(title="Takeout Times", color=COLOR_RED, description="")
-        currentDT = datetime.today().strftime('%A')
-
-        breakfast_time_str = "Breakfast: 7 AM – 11:30 AM \n"
-        lunch_time_str = "Lunch: 11:30 AM – 4 PM \n"
-        dinner_time_str = "Dinner: 4 PM – Midnight \n"
-        str_to_send = breakfast_time_str + lunch_time_str
-
-        if currentDT == "Monday":
-            str_to_send += dinner_time_str
-        elif currentDT == "Tuesday":
-            str_to_send += dinner_time_str
-        elif currentDT == "Wednesday":
-            str_to_send += dinner_time_str
-        elif currentDT == "Thursday":
-            str_to_send += dinner_time_str
-        elif currentDT == "Friday":
-            pass
-        elif currentDT == "Saturday":
-            str_to_send = "Saturday: Closed\n"
-        else:
-            str_to_send = "Sunday: 5:30 PM – 10 PM \n"
-
-        # str_to_send = breakfast_time_str + lunch_time_str + dinner_time_str + sunday_time_str
-
-        embed.add_field(name="These are the times:", value=str_to_send, inline=False)
+        current_datetime = datetime.now(tz=pytz.utc).astimezone(self.eastern_timezone)
+        rightnow = current_datetime.time()
+        breakfast_time = self.takeout_times["breakfast_time"]
+        lunch_time = self.takeout_times["lunch_time"]
+        weekday_dinner_time = self.takeout_times["weekday_dinner_time"]
+        sunday_dinner_time = self.takeout_times["sunday_dinner_time"]
+        if 4 >= current_datetime.weekday() >= 0:
+            if self.takeout_times["breakfast_start"] <= rightnow <= self.takeout_times["breakfast_end"]:
+                breakfast_time = f"**{breakfast_time}**"
+            elif self.takeout_times["lunch_start"] <= rightnow <= self.takeout_times["lunch_end"]:
+                lunch_time = f"**{lunch_time}**"
+            elif current_datetime.weekday() != 4 and self.takeout_times["weekday_dinner_start"] <= rightnow <= \
+                    self.takeout_times["weekday_dinner_end"]:
+                weekday_dinner_time = f"**{weekday_dinner_time}**"
+        elif current_datetime.weekday() == 6 and self.takeout_times["sunday_dinner_start"] <= rightnow <= \
+                self.takeout_times["sunday_dinner_end"]:
+            sunday_dinner_time = f"**{sunday_dinner_time}**"
+        embed = discord.Embed(title="Takeout Times", description="", color=COLOR_RED)
+        embed.add_field(name="Breakfast", value=breakfast_time, inline=False)
+        embed.add_field(name="Lunch", value=lunch_time, inline=False)
+        embed.add_field(name="Dinner", value=weekday_dinner_time + "\n" + sunday_dinner_time, inline=False)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def DineIn(self, ctx):
-        """
-        Weekdays: 7:00AM – 9:00PM
-        Breakfast: 7:00 – 11:00AM
-        Lunch: 11:00AM – 4:00PM
-        Dinner: 4:00 – 9:00PM
-
-        Weekends: 9:30AM – 8:00PM
-        Brunch: 9:30AM – 4:00PM
-        Dinner: 4:00 – 8:00PM
-        """
         embed = discord.Embed(title="Dining Times", description="", color=COLOR_RED)
-
-        currentDT = datetime.today().strftime('%A')
-        week_breakfast_str = "Breakfast: 7 AM – 11 AM\n"
-        week_lunch_str = "Lunch: 11 AM – 4 PM\n"
-        week_dinner_str = "Dinner: 4 PM – 9 PM\n"
-
-        weekend_brunch_str = "Brunch: 9:30 AM – 4 PM\n"
-        weekend_dinner_str = "Dinner: 4 PM – 8 PM\n"
-
-        if currentDT == "Saturday":
-            str_to_send = weekend_brunch_str + weekend_dinner_str
-        elif currentDT == "Sunday":
-            str_to_send = weekend_brunch_str + weekend_dinner_str
+        current_datetime = datetime.now(tz=pytz.utc).astimezone(self.eastern_timezone)
+        rightnow = current_datetime.time()
+        weekday_times = self.dinein_times["weekday_times"].copy()
+        weekend_times = self.dinein_times["weekend_times"].copy()
+        if 4 >= current_datetime.weekday() >= 0:
+            if self.dinein_times["weekday_breakfast_start"] <= rightnow <= self.dinein_times["weekday_breakfast_end"]:
+                weekday_times["weekday_breakfast_time"] = f"**{weekday_times['weekday_breakfast_time']}**"
+            elif self.dinein_times["weekday_lunch_start"] <= rightnow <= self.dinein_times["weekday_lunch_end"]:
+                weekday_times["weekday_lunch_time"] = f"**{weekday_times['weekday_lunch_time']}**"
+            elif self.dinein_times["weekday_dinner_start"] <= rightnow <= self.dinein_times["weekday_dinner_end"]:
+                weekday_times["weekday_dinner_time"] = f"**{weekday_times['weekday_dinner_time']}**"
         else:
-            str_to_send = week_breakfast_str + week_lunch_str + week_dinner_str
-
-        embed.add_field(name="These are the times:", value=str_to_send, inline=False)
+            if self.dinein_times["weekend_brunch_start"] <= rightnow <= self.dinein_times["weekend_brunch_end"]:
+                weekend_times["weekend_brunch_time"] = f"**{weekend_times['weekend_brunch_time']}**"
+            elif self.dinein_times["weekend_dinner_start"] <= rightnow <= self.dinein_times["weekend_dinner_end"]:
+                weekend_times["weekend_dinner_time"] = f"**{weekend_times['weekend_dinner_time']}**"
+        weekday_str = ""
+        weekend_str = ""
+        for key, val in weekday_times.items():
+            weekday_str += val + "\n"
+        for key, val in weekend_times.items():
+            weekend_str += val + "\n"
+        embed.add_field(name="Weekdays", value=weekday_str, inline=False)
+        embed.add_field(name="Weekends", value=weekend_str, inline=False)
         await ctx.send(embed=embed)
 
 
